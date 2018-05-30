a91f14707872099618f6f885a763ac264a43746d
Skayo <jonasg.cool@gmail.com>
2018-05-30 17:50:34 +0200

    Added DB class
    Still needs some work

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
 // CONFIGURATION
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+define('DB_PORT', 3306);
+
 define('TEMPLATES', true); // Enable/Disable templates. When set to false, features like View::load or layouts wont work (except error layouts)
+
 define('ROUTES_DIR', 'routes/');
 define('VIEWS_DIR', 'views/');
 define('LAYOUTS_DIR', 'layouts/');
+define('QUERIES_DIR', 'queries/');
 define('CORE_DIR', 'core/');
+
 
 // Run
 require_once CORE_DIR . 'slid.php';
